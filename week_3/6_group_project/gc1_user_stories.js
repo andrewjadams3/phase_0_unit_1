471416e41dcda1b548547469058db4308134e3d0
Andrew Adams <andrewjadams3@gmail.com>
2014-05-06 08:38:06 -0500

    Add user stories

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,21 @@
+// As a user, I want to find the sum, the mean, and the median of a list of
+// numbers. These should be three separate functions.
+
+// The first function, sum, should add up every number in a list and provide the
+// total. For example, for a list containing the numbers 3, 4, and 5, the sum
+// function should provide the answer 12.
+
+// The second function, mean, should provide the mean (the average) of a list of
+// numbers. Again, for a list containing the numbers 3, 4, and 5, the mean
+// function should provide the answer 4.
+
+// Finally, the median function should find the median (the middle number) of a 
+// list of numbers. For a list containing the numbers 3, 4, and 5, the median
+// function should provide the answer 4. However, for a list containing the
+// numbers 2, 3, 4, and 5, the median function should provide the answer 3.5.
+
+// You can assume that lists of numbers will be already be sorted from smallest
+// to largest.
+
+// Each function should should work properly when given a list with an even
+// number of items, as well as a list with an odd number of items.
