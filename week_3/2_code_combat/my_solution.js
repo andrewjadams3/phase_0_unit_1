a6801ebf4abc84de51fc0c69298f97be4eec3be9
Andrew Adams <andrewjadams3@gmail.com>
2014-05-07 22:18:09 -0500

    Complete code combat beginner campaign and reflection

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,105 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself]
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// 1: Rescue Mission
+// move down x2
+// move right
+// move up x2
+// move right x2
+// move down x2
+// attack enemy
+
+// 2: Grab the Mushroom
+// move up
+// move right
+// move left
+// move up
+// attack enemy
+
+// 3: Drink Me
+// move right
+// attack enemy
+// move right
+// move down
+// move up
+// move right
+// attack enemy
+
+// 4: Taunt the Guards
+// move right
+// bust down door
+// move right
+// say "hey"
+// move left x2
+// say "attack"
+// move right
+// say "follow me"
+// move right x2
+// move up
+// move right
+// say "something"
+// move down
+// move right
+// move up
+// move right
+
+// 5: It's a Trap
+// move down x2
+// say "hello"
+// move up x2
+
+// 6: Break the Prison
+// if William, true
+// if Lucas, true
+// if Marcus, true
+// if Robert, true
+// if Gordon, true
+// else false
+
+// 7: Taunt
+// say "Soon you'll be wearing my sword like a shish kebab!"
+// say "Have you stopped wearing diapers yet?"
+// say "I've spoken with apes more polite then you."
+// say "You fight like a dairy farmer."
+
+// 8: Cowardly Taunt
+// move(coordinate)
+// say "taunt"
+// move to safe spot
+
+// 9: Commanding Followers
+// say "follow"
+// move to enemies
+// say "attack"
+// move back
+
+// 10: Mobile Artillery
+// move down
+// attack coordinate
+// move up
+// attack coordinate x2
+// attack new coordinate x2
  
- 
- 
- 
- 
- 
- 
- 
+
+// Questions
+// 1: This is referring to the character (or object) whose methods are being
+// called. Similar to 'self' in Ruby
+// 2: The parens are where arguments for a function or method are placed.
+// 3: The semicolons are used at the end of statements. Similar to a period or
+// a newline.
+
 // Reflection:
-// Write your reflection here.
+// I didn't run into any major issues during this challenge. Often, my
+// pseudocode didn't quite line up with what the game wanted (e.g. not enough
+// moves in a direction), but that was easily fixed.
+// I didn't have any burning questions or difficult concepts. I found these
+// first missions to be very simple.
+// I didn't really enjoy the challenge. At the beginning, it was quite obvious
+// how to complete each one. The difficiult part was working around the
+// interface, which seemed to randomly refresh and call spells which weren't
+// ready. I spent more time trying to trick the game into running properly 
+// than writing code or coming up with solutions. I would hope that the game
+// gets more responsive and challenging syntactically as you progress.
